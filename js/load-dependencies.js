fa4848f86090f7db391d13f08195619dfbed2187
Ben <benlinux915@gmail.com>
2023-08-30 07:43:44 -0400

    Updates

--- js/load-dependencies.js
+++ js/load-dependencies.js
@@ -1,14 +1,15 @@
 function loadToolDependencies() {
 
-    var dependency;
-    var element = document.getElementsByTagName("body")[0]
+  var dependency;
+  var element = document.getElementsByTagName('body')[0];
 
-    console.log('loading csf-waterdef-v4 css');
-    dependency = document.createElement('link');
-    dependency.setAttribute("rel","stylesheet");
-    dependency.setAttribute("type","text/css");
-    dependency.setAttribute("href", CSFTOOL_URL + "/style/csf-waterdef-v4.css?v=4.0.0");
-    element.appendChild(dependency);
+  console.log('loading csf-waterdef-v4 css');
+  dependency = document.createElement('link');
+  dependency.setAttribute('rel','stylesheet');
+  dependency.setAttribute('type','text/css');
+  // eslint-disable-next-line no-undef
+  dependency.setAttribute('href', CSFTOOL_URL + '/style/csf-waterdef-v4.css?v=4.0.0');
+  element.appendChild(dependency);
 
 }
 loadToolDependencies();
